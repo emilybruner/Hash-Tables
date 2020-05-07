de223c80b945db9bc28eed14939fa7b7f9e1c472
Emily Bruner <bruner.emilym@gmail.com>
2020-05-07 19:56:24 -0400

    histogram finished

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,16 +1,23 @@
 def no_dups(s):
     # Implement me.
-    index = 0
-    for i in range(0, s:
-        for j in range(0, i + 1):
-            if (s[i] == s[j]):
-                break
+    storage = dict()
+
+# Loop over the input string, assign each word in the string to a dictionary key
+# for word in storage
+    for word in s:
+        if word not in storage:
+            storage[word]
+    # index = 0
+    # for i in range(0, s):
+    #     for j in range(0, i + 1):
+    #         if (s[i] == s[j]):
+    #             break
         
-        if (j == i):
-            s[index] = s[i]
-            index += 1
+    #     if (j == i):
+    #         s[index] = s[i]
+    #         index += 1
     
-    return "".join(s[:index])
+    # return "".join(s[:index])
 
 
 if __name__ == "__main__":
